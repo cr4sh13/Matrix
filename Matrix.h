744b3b59e715dc517561a9a9ba8011f89ed7cd9c
salvatore <totobaglieri95@gmail.com>
2018-08-21 20:29:45 +0200

    Add exception

--- Matrix.h
+++ Matrix.h
@@ -7,6 +7,8 @@
 
 
 #include <iostream>
+#include <cmath>
+#include <stdexcept>
 
 template<typename T>
 class Matrix {
@@ -37,7 +39,7 @@ public:
         std::cout << "\n";
         for (int i = 1; i <= row; i++) {
             for (int j = 1; j <= columns; j++) {
-                std::cout << this->getValue(i, j) << "\t";
+                std::cout << "(" << this->getValue(i, j) << ")" << "\t";
 
             }
             std::cout << std::endl << std::endl;
@@ -46,6 +48,14 @@ public:
         std::cout << "\n\n";
     }
 
+    virtual void insertRandomValues() {
+        srand((unsigned) time(0));
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < columns; j++) {
+                value[j + i * columns] = (rand() % 10) + 1;
+            }
+        }
+    }
 
     void insertValues() {
         std::cout << std::endl;
@@ -60,14 +70,19 @@ public:
 
     }
 
-    virtual T getValue(int x, int y) const {
+    virtual T getValue(int x, int y) const throw(std::out_of_range) {
         if (((x > 0) && (x <= row)) && ((y > 0) && (y <= columns)))
             return value[(x - 1) + (y - 1) * row];
+        else
+            throw (std::out_of_range("The matrix has different dimensions"));
+
     }
 
-    virtual void setValue(const int x, const int y, const T val) {
+    virtual void setValue(const int x, const int y, const T val) throw(std::out_of_range) {
         if ((x > 0) && (y > 0) && (x <= row) && (y <= columns))
             value[(x - 1) + (y - 1) * row] = val;
+        else
+            throw (std::out_of_range("The matrix has different dimensions"));
     }
 
     virtual ~Matrix() {
@@ -75,27 +90,28 @@ public:
             delete[] value;
     }
 
-    virtual Matrix rowMatrix(const int x) {
+    virtual Matrix rowMatrix(const int x) throw(std::out_of_range) {
         Matrix tmp(1, this->row);
         if (x > 0 && x <= this->row) {
-            for (int i = 1; i <= this->row; i++)
+            for (int i = 1; i <= this->row; i++) {
                 tmp.setValue(1, i, this->getValue(x, i));
-        }
-
-        return tmp;
+            }
+            return tmp;
+        } else
+            throw (std::out_of_range("Row not exist"));
     }
 
-    virtual Matrix colMatrix(const int y) {
+    virtual Matrix colMatrix(const int y) throw(std::out_of_range) {
         Matrix tmp(this->columns, 1);
         if (y > 0 && y <= this->columns) {
-            for (int i = 1; i <= this->columns; i++)
+            for (int i = 1; i <= this->columns; i++) {
                 tmp.setValue(i, 1, this->getValue(i, y));
-        }
-
-        return tmp;
+            }
+            return tmp;
+        } else throw (std::out_of_range("Column not exist"));
     }
 
-    virtual Matrix &trasp() {
+    virtual Matrix &trasp() throw(std::out_of_range) {
         if (this->value) {
             Matrix tmp(this->columns, this->row);
             T tempor;
@@ -107,11 +123,12 @@ public:
                 }
             *this = tmp;
             return *this;
-        }
+        } else
+            throw (std::out_of_range("The Matrix does not indicate values"));
     }
 
 
-    virtual Matrix operator*(const Matrix &rh) const {
+    virtual Matrix operator*(const Matrix &rh) const throw(std::out_of_range) {
         Matrix<T> tmpMatrix(this->row, rh.columns);
         if (this->columns == rh.row) {
             for (int i = 1; i <= this->row; i++) {
@@ -124,11 +141,18 @@ public:
                 }
             }
             return tmpMatrix;
-        }
+        } else throw (std::out_of_range("Matrices can not be multiplied"));
+    }
+
+    Matrix<T> operator*(const T &scalare) {
+        Matrix<T> prodScalare(this->row, this->columns);
+        for (int i = 0; i < row * columns; i++)
+            prodScalare.value[i] = scalare * value[i];
+        return prodScalare;
     }
 
 
-    virtual Matrix operator+(const Matrix &rh) const {
+    virtual Matrix operator+(const Matrix &rh) const throw(std::out_of_range) {
         Matrix tmp(rh.row, rh.columns);
         if ((this->row == rh.row) && (this->columns == rh.columns)) {
             for (int i = 1; i <= this->row; i++)
@@ -136,7 +160,7 @@ public:
                     tmp.setValue(i, j, (this->getValue(i, j) + rh.getValue(i, j)));
                 }
             return tmp;
-        }
+        } else throw (std::out_of_range("Matrices can not be added"));
     }
 
     virtual Matrix &operator=(const Matrix &rh) {
--- main.cpp
+++ main.cpp
@@ -2,19 +2,81 @@
 #include "Matrix.h"
 #include "MatrixFactory.h"
 #include "MatrixType.h"
+#include <stdexcept>
 
-using namespace std;
 
 int main() {
 
-    MatrixFactory<int>* mtx1 = new MatrixType<int>();
-    Matrix<int>* matrix1 = mtx1->choiceMatrixType(3,3);
-    matrix1->insertValues();
-    matrix1->typeOfMatrix();
-    matrix1->printMatrix();
+    try {
+
+        std::string anwser;
+        int value, row, column;
+
+        MatrixFactory<int> *intFactory = new MatrixType<int>();
+        Matrix<int> *imatrix = intFactory->choiceMatrixType(2, 2);
+        std::cout << "MATRIX OF INTEGERS";
+        imatrix->insertRandomValues();
+        imatrix->printMatrix();
+        std::cout << std::endl;
+
+        MatrixFactory<float> *floatFactory = new MatrixType<float>;
+        std::cout << "FIRST MATRIX OF FLOATS";
+        Matrix<float> *fmatrix = floatFactory->choiceMatrixType(2, 2);
+        fmatrix->insertRandomValues();
+        fmatrix->printMatrix();
+        std::cout << std::endl;
 
 
+        std::cout << "SECOND MATRIX OF FLOATS";
+        Matrix<float> *fsmatrix = floatFactory->choiceMatrixType(2, 2);
+        fsmatrix->insertRandomValues();
+        fsmatrix->printMatrix();
+        std::cout << std::endl;
 
+        std::cout << "MOLTIPLICATION OF FLOAT MATRIX";
+        Matrix<float> *rs = floatFactory->choiceMatrixType(2, 2);
+        (*rs) = (*fmatrix) * (*fsmatrix);
+        rs->printMatrix();
+
+
+        std::cout << "TRANSPOSE OF INT MATRIX";
+        imatrix->trasp();
+        imatrix->printMatrix();
+
+
+        Matrix<int> *firstMatrix = intFactory->choiceMatrixType(2, 2);
+        std::cout << "copying int matrix to first matrix..." << std::endl;
+        firstMatrix = imatrix;
+        std::cout << "\nFIRST MATRIX";
+        firstMatrix->printMatrix();
+
+        Matrix<int> *secondMatrix = intFactory->choiceMatrixType(2, 3);
+        secondMatrix->insertRandomValues();
+
+        Matrix<int> *resultMatrix = intFactory->choiceMatrixType(2, 3);
+
+        std::cout << "MOLTIPLICATION OF TWO MATRIX";
+        *resultMatrix = (*firstMatrix) * (*secondMatrix);
+        resultMatrix->printMatrix();
+
+
+        std::cout << "\nMODIFYING MATRIX ELEMENT" << std::endl;
+
+        std::cout << "Insert new value: ";
+        std::cin >> value;
+        std::cout << "Insert target's row: ";
+        std::cin >> row;
+        std::cout << "Insert target's columns: ";
+        std::cin >> column;
+        resultMatrix->setValue(row, column, value);
+
+        std::cout << "NEW MODIFIED MATRIX";
+        resultMatrix->printMatrix();
+    }
+    catch (std::out_of_range &e) {
+        std::cout << e.what() << std::endl;
+        return 36;
+    }
 
     return 0;
 }
